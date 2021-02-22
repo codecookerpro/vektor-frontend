b2d30971899525a99ae9eb3e1a400823b081908b
Great Dolphin <great.dolphin.ls@gmail.com>
2021-02-23 04:39:55 +0700

    [feat] refactor Sidebar component

--- src/layouts/Dashboard/index.js
+++ src/layouts/Dashboard/index.js
@@ -1,17 +1,17 @@
-import React, { memo, useState } from "react";
-import styled, { createGlobalStyle } from "styled-components/macro";
-import { spacing } from "@material-ui/system";
+import React, { memo, useState } from 'react';
+import styled, { createGlobalStyle } from 'styled-components/macro';
+import { spacing } from '@material-ui/system';
 import {
   Hidden,
   CssBaseline,
   Paper as MuiPaper,
   withWidth,
-} from "@material-ui/core";
-import { isWidthUp } from "@material-ui/core/withWidth";
+} from '@material-ui/core';
+import { isWidthUp } from '@material-ui/core/withWidth';
 
-import Sidebar from "components/Sidebar";
-import Header from "components/AppBar";
-import Footer from "components/Footer";
+import Header from 'components/AppBar';
+import Footer from 'components/Footer';
+import Sidebar from './Sidebar';
 
 const drawerWidth = 258;
 
@@ -39,7 +39,7 @@ const Root = styled.div`
 `;
 
 const Drawer = styled.div`
-  ${(props) => props.theme.breakpoints.up("md")} {
+  ${(props) => props.theme.breakpoints.up('md')} {
     width: ${drawerWidth}px;
     flex-shrink: 0;
   }
@@ -78,16 +78,16 @@ const Dashboard = ({ children, routes, width }) => {
       <CssBaseline />
       <GlobalStyle />
       <Drawer>
-        <Hidden mdUp implementation="js">
+        <Hidden mdUp implementation='js'>
           <Sidebar
             routes={routes}
             PaperProps={{ style: { width: drawerWidth } }}
-            variant="temporary"
+            variant='temporary'
             open={mobileOpen}
             onClose={handleDrawerToggle}
           />
         </Hidden>
-        <Hidden smDown implementation="css">
+        <Hidden smDown implementation='css'>
           <Sidebar
             routes={routes}
             PaperProps={{ style: { width: drawerWidth } }}
@@ -96,7 +96,7 @@ const Dashboard = ({ children, routes, width }) => {
       </Drawer>
       <AppContent>
         <Header onDrawerToggle={handleDrawerToggle} />
-        <MainContent p={isWidthUp("lg", width) ? 12 : 5}>
+        <MainContent p={isWidthUp('lg', width) ? 12 : 5}>
           {children}
         </MainContent>
         <Footer />
