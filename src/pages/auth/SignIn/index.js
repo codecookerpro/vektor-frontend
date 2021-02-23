0606d5dd83f9642771792d6d0fbfd30f54d21eab
Great Dolphin <great.dolphin.ls@gmail.com>
2021-02-23 08:38:46 +0700

    [feat] implement Logo component

--- src/pages/auth/SignIn/index.js
+++ src/pages/auth/SignIn/index.js
@@ -1,11 +1,11 @@
-import React, { memo } from "react";
-import { useDispatch } from "react-redux";
-import { useHistory } from "react-router-dom";
-import styled from "styled-components/macro";
-import { Link } from "react-router-dom";
-import { Helmet } from "react-helmet";
-import * as yup from "yup";
-import { Formik } from "formik";
+import React, { memo } from 'react';
+import { useDispatch } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+import styled from 'styled-components/macro';
+import { Link } from 'react-router-dom';
+import { Helmet } from 'react-helmet';
+import * as yup from 'yup';
+import { Formik } from 'formik';
 import {
   Avatar,
   Checkbox,
@@ -14,11 +14,11 @@ import {
   Paper,
   TextField as MuiTextField,
   Typography,
-} from "@material-ui/core";
-import { spacing } from "@material-ui/system";
-import { Alert as MuiAlert } from "@material-ui/lab";
+} from '@material-ui/core';
+import { spacing } from '@material-ui/system';
+import { Alert as MuiAlert } from '@material-ui/lab';
 
-import { signIn } from "redux/actions/authActions";
+import { signIn } from 'redux/actions/authActions';
 import {
   EMAIL_VALID,
   PASSWORD_VALID
@@ -30,7 +30,7 @@ const TextField = styled(MuiTextField)(spacing);
 
 const Wrapper = styled(Paper)`
   padding: ${(props) => props.theme.spacing(6)}px;
-  ${(props) => props.theme.breakpoints.up("md")} {
+  ${(props) => props.theme.breakpoints.up('md')} {
     padding: ${(props) => props.theme.spacing(10)}px;
   }
 `;
@@ -53,20 +53,20 @@ function SignIn() {
 
   return (
     <Wrapper>
-      <Helmet title="Sign In" />
-      <BigAvatar alt="Lucy" src="/static/img/avatars/avatar-1.jpg" />
+      <Helmet title='Sign In' />
+      <BigAvatar alt='Lucy' src='/static/img/avatars/avatar-1.jpg' />
 
-      <Typography component="h1" variant="h4" align="center" gutterBottom>
+      <Typography component='h1' variant='h4' align='center' gutterBottom>
         Welcome back, Lucy!
       </Typography>
-      <Typography component="h2" variant="body1" align="center">
+      <Typography component='h2' variant='body1' align='center'>
         Sign in to your account to continue
       </Typography>
 
       <Formik
         initialValues={{
-          email: "demo@bootlab.io",
-          password: "unsafepassword",
+          email: 'demo@bootlab.io',
+          password: 'unsafepassword',
           submit: false,
         }}
         validationSchema={schema}
@@ -75,9 +75,9 @@ function SignIn() {
             await dispatch(
               signIn({ email: values.email, password: values.password })
             );
-            history.push("/dashboard/default");
+            history.push('/dashboard/default');
           } catch (error) {
-            const message = error.message || "Something went wrong";
+            const message = error.message || 'Something went wrong';
 
             setStatus({ success: false });
             setErrors({ submit: message });
@@ -95,19 +95,19 @@ function SignIn() {
           values,
         }) => (
           <form noValidate onSubmit={handleSubmit}>
-            <Alert mt={3} mb={1} severity="info">
-              Use <strong>demo@bootlab.io</strong> and{" "}
+            <Alert mt={3} mb={1} severity='info'>
+              Use <strong>demo@bootlab.io</strong> and{' '}
               <strong>unsafepassword</strong> to sign in
             </Alert>
             {errors.submit && (
-              <Alert mt={2} mb={1} severity="warning">
+              <Alert mt={2} mb={1} severity='warning'>
                 {errors.submit}
               </Alert>
             )}
             <TextField
-              type="email"
-              name="email"
-              label="Email Address"
+              type='email'
+              name='email'
+              label='Email Address'
               value={values.email}
               error={Boolean(touched.email && errors.email)}
               fullWidth
@@ -117,9 +117,9 @@ function SignIn() {
               my={2}
             />
             <TextField
-              type="password"
-              name="password"
-              label="Password"
+              type='password'
+              name='password'
+              label='Password'
               value={values.password}
               error={Boolean(touched.password && errors.password)}
               fullWidth
@@ -129,23 +129,23 @@ function SignIn() {
               my={2}
             />
             <FormControlLabel
-              control={<Checkbox value="remember" color="primary" />}
-              label="Remember me"
+              control={<Checkbox value='remember' color='primary' />}
+              label='Remember me'
             />
             <Button
-              type="submit"
+              type='submit'
               fullWidth
-              variant="contained"
-              color="primary"
+              variant='contained'
+              color='primary'
               disabled={isSubmitting}
             >
               Sign in
             </Button>
             <Button
               component={Link}
-              to="/auth/reset-password"
+              to='/auth/reset-password'
               fullWidth
-              color="primary"
+              color='primary'
             >
               Forgot password
             </Button>
---
+++ src/utils/constants/image-paths.js
@@ -0,0 +1,6 @@
+
+const LOGO_IMAGE_PATH = '/static/img/logo.png';
+
+export {
+  LOGO_IMAGE_PATH,
+};
