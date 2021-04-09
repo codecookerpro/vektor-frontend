da9bf2d0fbe254880a0b2fd22fd2e2724f78b24c
Great Dolphin <great.dolphin.ls@gmail.com>
2021-04-09 01:44:35 -0700

    [feat] create UserPages

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -2,12 +2,14 @@ import { combineReducers } from 'redux'
 
 import loadingReducer from './loadingReducer'
 import authReducer from './authReducer'
+import usersReducer from './usersReducer'
 import organizationsReducer from './organizationsReducer'
 import workflowTemplatesReducer from './workflowTemplatesReducer'
 
 export default combineReducers({
   loading: loadingReducer,
   auth: authReducer,
+  users: usersReducer,
   organizations: organizationsReducer,
   workflowTemplates: workflowTemplatesReducer
 });
