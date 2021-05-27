cebbfbf2a5d3983b7544b671fb10aae8b522bd29
sa44as <sa44as44@gmail.com>
2021-05-28 02:16:09 +0400

    dupicate key fix

--- src/pages/WorkflowTemplates/WorkflowTemplateChart/CustomFlowNode.js
+++ src/pages/WorkflowTemplates/WorkflowTemplateChart/CustomFlowNode.js
@@ -14,136 +14,156 @@ import CloseIcon from '@material-ui/icons/Close';
 const GlobalCss = withStyles({
   // @global is handled by jss-plugin-global.
   '@global': {
-	// You should target [class*='MuiButton-root'] instead if you nest themes.
-	'.MuiInputBase-root::before, .MuiInputBase-root::after': {
-		display: 'none',
-	},
+    // You should target [class*='MuiButton-root'] instead if you nest themes.
+    '.MuiInputBase-root::before, .MuiInputBase-root::after': {
+      display: 'none',
+    },
   },
 })(() => null);
 
 const useStyles = makeStyles((theme) => ({
-	nodeContent: {
-		width: 'calc(100% - 24px)',
-		height: 'calc(100% - 14px)',
-		display: 'flex',
-		alignItems: 'center',
-		justifyContent: 'center',
-		position: 'absolute',
-		top: '7px',
-		left: '12px',
-		textAlign: 'center',
-	},
-	handle: {
-		background: '#555',
-	},
-	name: {
-		width: '100%',
-		margin: '0',
-		overflowX: 'hidden',
-		overflowY: 'auto',
-		textOverflow: 'ellipsis',
-		maxHeight: '100%',
-	},
-	nodePopupContainer: {
-		zIndex:100,
-	},
-	nodePopup: {
-		marginTop: '50px',
-		minHeight: '100px',
-		padding: theme.spacing(1),
-		backgroundColor: 'white',
-		display: 'flex',
-		flexDirection: 'column',
-		padding: '24px 30px',
-	},
-	nodePopupButton: {
-		width: '100%',
-		marginBottom: '12px',
-	},
-	nodePopupTextAlign: {
-		textAlign: 'center',
-		marginBottom: '12px',
-	},
-	nodePopupCloseIcon: {
-		position: 'relative',
-		left: '100%',
-		cursor: 'pointer',
-	},
+  nodeContent: {
+    width: 'calc(100% - 24px)',
+    height: 'calc(100% - 14px)',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    position: 'absolute',
+    top: '7px',
+    left: '12px',
+    textAlign: 'center',
+  },
+  handle: {
+    background: '#555',
+  },
+  name: {
+    width: '100%',
+    margin: '0',
+    overflowX: 'hidden',
+    overflowY: 'auto',
+    textOverflow: 'ellipsis',
+    maxHeight: '100%',
+  },
+  nodePopupContainer: {
+    zIndex: 100,
+  },
+  nodePopup: {
+    marginTop: '50px',
+    minHeight: '100px',
+    backgroundColor: 'white',
+    display: 'flex',
+    flexDirection: 'column',
+    padding: '24px 30px',
+  },
+  nodePopupButton: {
+    width: '100%',
+    marginBottom: '12px',
+  },
+  nodePopupTextAlign: {
+    textAlign: 'center',
+    marginBottom: '12px',
+  },
+  nodePopupCloseIcon: {
+    position: 'relative',
+    left: '100%',
+    cursor: 'pointer',
+  },
 }));
 
 export default memo(({ data }) => {
-	const classes = useStyles();
-	const [name, setName] = useState(null);
-	const [nameTemp, setNameTemp] = useState(null);
-	const [popperElement, setPopperElement] = useState(null);
-	const popperOpen = Boolean(popperElement);
-	const [dialogOpen, setDialogOpen] = useState(false);
+  const classes = useStyles();
+  const [name, setName] = useState(null);
+  const [nameTemp, setNameTemp] = useState(null);
+  const [popperElement, setPopperElement] = useState(null);
+  const popperOpen = Boolean(popperElement);
+  const [dialogOpen, setDialogOpen] = useState(false);
 
-	const handlePopUpClick = (e) => {
-		e.preventDefault();
-		setPopperElement(popperElement ? null : e.currentTarget);
-		data.handleSwitchPopup(popperElement ? null : e.currentTarget);
-	}
+  const handlePopUpClick = (e) => {
+    e.preventDefault();
+    setPopperElement(popperElement ? null : e.currentTarget);
+    data.handleSwitchPopup(popperElement ? null : e.currentTarget);
+  };
 
-	const handleNameUpdate = (e) => {
-		e.preventDefault();
-		setNameTemp(e.target.value);
-	}
+  const handleNameUpdate = (e) => {
+    e.preventDefault();
+    setNameTemp(e.target.value);
+  };
 
-	const handleDialogClickOpen = () => {
-		setDialogOpen(true);
-	};
+  const handleDialogClickOpen = () => {
+    setDialogOpen(true);
+  };
 
-	const handleDialogCancel = () => {
-		setDialogOpen(false);
-	};
+  const handleDialogCancel = () => {
+    setDialogOpen(false);
+  };
 
-	const handleDialogApprove = (e) => {
-		e.preventDefault();
-		setDialogOpen(false);
-		setName(nameTemp);
-		data.handleInputChange(data.id, nameTemp);
-	};
+  const handleDialogApprove = (e) => {
+    e.preventDefault();
+    setDialogOpen(false);
+    setName(nameTemp);
+    data.handleInputChange(data.id, nameTemp);
+  };
 
-	return (
-		<>
-			<GlobalCss />
-			<div onDoubleClick={(e) => handlePopUpClick(e)}>
-				<Handle type='target' style={{ background: '#557fb9' }} position='top' id={('top-' + data.id)} className={classes.handle} />
-				<Handle type='target' style={{ background: '#557fb9' }} position='left' id={('left-' + data.id)} className={classes.handle} />
-					<div className={classes.nodeContent}>
-						{name ? <p className={classes.name}>{name}</p> : (
-							<p className={classes.name}><small>Double-click to edit</small></p>
-						)}
-					</div>
-				<Handle type='source' style={{ background: '#f9037ede' }} position='bottom' id={('bottom-' + data.id)} className={classes.handle} />
-				<Handle type='source' style={{ background: '#f9037ede' }} position='right' id={('right-' + data.id)} className={classes.handle} />
-				<Popper open={popperOpen} anchorEl={popperElement} className={classes.nodePopupContainer} transition>
-					{({ TransitionProps }) => (
-						<Fade {...TransitionProps}>
-							<div className={classes.nodePopup}>
-								<CloseIcon className={classes.nodePopupCloseIcon} onClick={(e) => handlePopUpClick(e)} />
-								<Button variant='contained' color='primary' className={classes.nodePopupButton} onClick={handleDialogClickOpen} >
-									Edit
-								</Button>
-								<Button variant='contained' color='default' onClick={(e) => data.handleDeleteNode(data.id, e)}>
-									Delete
-								</Button>
-							</div>
-						</Fade>
-					)}
-				</Popper>
-			</div>
-			<Dialog open={dialogOpen} onClose={handleDialogCancel} aria-labelledby='alert-dialog-title' aria-describedby='alert-dialog-description' >
-				<DialogTitle id='alert-dialog-title' className={classes.nodePopupTextAlign} >{'EDIT'}</DialogTitle>
-				<DialogContent>
-					<TextField onChange={handleNameUpdate} label={data.label} defaultValue={name} className={classes.textField} multiline={true} rows={2} classes={{root: classes.root}} autoFocus={true} />
-				</DialogContent>
-				<DialogActions>
-					<Button onClick={handleDialogCancel} color='primary'> Cancel </Button>
-					<Button onClick={handleDialogApprove} color='primary' autoFocus> Approve </Button>
-				</DialogActions>
-			</Dialog>
-		</>
-	);
+  return (
+    <>
+      <GlobalCss />
+      <div onDoubleClick={(e) => handlePopUpClick(e)}>
+        <Handle type="target" style={{ background: '#557fb9' }} position="top" id={'top-' + data.id} className={classes.handle} />
+        <Handle type="target" style={{ background: '#557fb9' }} position="left" id={'left-' + data.id} className={classes.handle} />
+        <div className={classes.nodeContent}>
+          {name ? (
+            <p className={classes.name}>{name}</p>
+          ) : (
+            <p className={classes.name}>
+              <small>Double-click to edit</small>
+            </p>
+          )}
+        </div>
+        <Handle type="source" style={{ background: '#f9037ede' }} position="bottom" id={'bottom-' + data.id} className={classes.handle} />
+        <Handle type="source" style={{ background: '#f9037ede' }} position="right" id={'right-' + data.id} className={classes.handle} />
+        <Popper open={popperOpen} anchorEl={popperElement} className={classes.nodePopupContainer} transition>
+          {({ TransitionProps }) => (
+            <Fade {...TransitionProps}>
+              <div className={classes.nodePopup}>
+                <CloseIcon className={classes.nodePopupCloseIcon} onClick={(e) => handlePopUpClick(e)} />
+                <Button variant="contained" color="primary" className={classes.nodePopupButton} onClick={handleDialogClickOpen}>
+                  Edit
+                </Button>
+                <Button variant="contained" color="default" onClick={(e) => data.handleDeleteNode(data.id, e)}>
+                  Delete
+                </Button>
+              </div>
+            </Fade>
+          )}
+        </Popper>
+      </div>
+      <Dialog open={dialogOpen} onClose={handleDialogCancel} aria-labelledby="alert-dialog-title" aria-describedby="alert-dialog-description">
+        <DialogTitle id="alert-dialog-title" className={classes.nodePopupTextAlign}>
+          {'EDIT'}
+        </DialogTitle>
+        <DialogContent>
+          <TextField
+            onChange={handleNameUpdate}
+            label={data.label}
+            defaultValue={name}
+            className={classes.textField}
+            multiline={true}
+            rows={2}
+            classes={{ root: classes.root }}
+            autoFocus={true}
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleDialogCancel} color="primary">
+            {' '}
+            Cancel{' '}
+          </Button>
+          <Button onClick={handleDialogApprove} color="primary" autoFocus>
+            {' '}
+            Approve{' '}
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </>
+  );
 });
