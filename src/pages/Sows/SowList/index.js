3be5e4acc7f18749717b5d020eaa3dfc4651fa73
ryzh_vii <ryzh.vii@gmail.com>
2021-06-04 21:36:21 +0300

    Added SOW table

--- src/pages/Sows/SowList/index.js
+++ src/pages/Sows/SowList/index.js
@@ -1,5 +1,10 @@
 import React, { memo } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
+import PageHeader from '../../../parts/PageHeader';
+import LINKS from '../../../utils/constants/links';
+import OrganizationFilter from '../../Projects/ProjectList/OrganizationFilter';
+import ProjectsTable from '../../Projects/ProjectList/ProjectsTable';
+import SowTable from './SowTable';
 
 const useStyles = makeStyles(() => ({
   root: {
@@ -12,7 +17,13 @@ const useStyles = makeStyles(() => ({
 const SowList = () => {
   const classes = useStyles();
 
-  return <main className={classes.root}>Sow List Page</main>;
+  return (
+    <>
+      {/*<PageHeader title={LINKS.PROJECTS.TITLE} links={NAV_LINKS} leftElement={isVisible && renderAddProjectButton()} />*/}
+      {/*<OrganizationFilter organization={organization} setOrganization={setOrganization} />*/}
+      <SowTable />
+    </>
+  );
 };
 
 export default memo(SowList);
