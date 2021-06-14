dfdeb66cfdcce593299edf6b047129ba3884ca40
ryzh_vii <ryzh.vii@gmail.com>
2021-06-14 15:01:14 +0300

    Updated view table SOWs without organization for users

---
+++ src/pages/Sows/SowList/SowTable/setColumn.js
@@ -0,0 +1,21 @@
+const setColumn = (visible) =>
+  visible
+    ? [
+        { id: 'Checkbox', label: 'checkbox', minWidth: 100 },
+        { id: 'organization', label: 'Organization', minWidth: 170 },
+        { id: 'name', label: 'Name', minWidth: 170 },
+        { id: 'system', label: 'System', minWidth: 170 },
+        { id: 'fullContactName', label: 'Full Contact Name', minWidth: 170 },
+        { id: 'projectName', label: 'Project Name', minWidth: 170 },
+        { id: 'vendorName', label: 'Vendor Name', minWidth: 170 },
+      ]
+    : [
+        { id: 'Checkbox', label: 'checkbox', minWidth: 100 },
+        { id: 'name', label: 'Name', minWidth: 170 },
+        { id: 'system', label: 'System', minWidth: 170 },
+        { id: 'fullContactName', label: 'Full Contact Name', minWidth: 170 },
+        { id: 'projectName', label: 'Project Name', minWidth: 170 },
+        { id: 'vendorName', label: 'Vendor Name', minWidth: 170 },
+      ];
+
+export default setColumn;
