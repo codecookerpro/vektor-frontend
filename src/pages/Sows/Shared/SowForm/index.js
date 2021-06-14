201b9e88057efb73513f61c4dd8648cf1bd7c69f
ryzh_vii <ryzh.vii@gmail.com>
2021-06-14 13:51:42 +0300

    Added condition for hiding/show organization

--- src/pages/Sows/Shared/SowForm/index.js
+++ src/pages/Sows/Shared/SowForm/index.js
@@ -1,32 +1,31 @@
 import React, { memo, useState } from 'react';
 import { useHistory } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { useForm } from 'react-hook-form';
 import { joiResolver } from '@hookform/resolvers/joi';
-import joi from 'joi';
 import { Button } from '@material-ui/core';
 import NewSow from './NewSow';
 import InitiationPhase from './InitiationPhase';
 import IntakePhase from './IntakePhase';
 import IproSubmit from './IproSubmit';
-import { SELECT_VALID, STRING_INPUT_VALID } from 'utils/constants/validations';
 import { SOW_FORM_PHASE } from 'utils/constants/sowFormPhase';
 import LINKS from 'utils/constants/links';
 import { useStyles } from './styles';
 import { addSOW } from 'redux/actions/sowAction';
-
-const schema = joi.object().keys({
-  name: STRING_INPUT_VALID,
-  organization: SELECT_VALID,
-  metaSystem: SELECT_VALID,
-});
+import { PERMISSION_TYPE } from 'utils/constants/permissions';
+import setSchema from './setSchema';
 
 const SowForm = ({ mode }) => {
   const classes = useStyles();
   const history = useHistory();
   const dispatch = useDispatch();
 
-  const { control, handleSubmit, errors, reset, watch, setValue } = useForm({
+  const { permissions } = useSelector(({ auth }) => auth.currentUser);
+  const isOrganizationVisible = permissions === PERMISSION_TYPE.ADMIN;
+
+  const schema = setSchema(isOrganizationVisible);
+
+  const { control, handleSubmit, errors } = useForm({
     resolver: joiResolver(schema),
     shouldUnregister: false,
   });
@@ -46,11 +45,20 @@ const SowForm = ({ mode }) => {
     await dispatch(addSOW(params));
     history.push(LINKS.SOWS.HREF);
   };
+  console.log('error', errors);
 
   return (
     <>
       <form noValidate className={classes.form} onSubmit={handleSubmit(onSubmit)}>
-        <NewSow mode={mode} sow={{}} control={control} errors={errors} project={project} setProject={setProject} />
+        <NewSow
+          mode={mode}
+          sow={{}}
+          control={control}
+          errors={errors}
+          project={project}
+          setProject={setProject}
+          isOrganizationVisible={isOrganizationVisible}
+        />
         <InitiationPhase mode={mode} sow={{}} title={SOW_FORM_PHASE.INITIATION_PHASE} setNotRequiredField={setNotRequiredField} />
         <IntakePhase mode={mode} sow={{}} title={SOW_FORM_PHASE.INTAKE_PHASE} setNotRequiredField={setNotRequiredField} />
         <IproSubmit mode={mode} sow={{}} title={SOW_FORM_PHASE.IPRO_SUBMIT} setNotRequiredField={setNotRequiredField} />
