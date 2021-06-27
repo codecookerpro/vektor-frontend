c7ae04c0d56347fb49ecbf34631d15db9055bc89
unknown <steve.jin.md@gmail.com>
2021-06-28 06:48:40 +0900

    [update] user list view

--- src/utils/hooks/index.js
+++ src/utils/hooks/index.js
@@ -2,3 +2,4 @@ export { default as useAllowedRoutes } from './useAllowedRoutes';
 export { default as useLoading } from './useLoading';
 export { default as usePagenation } from './usePagenation';
 export { default as useFilter } from './useFilter';
+export { default as useUserPermission } from './useUserPermission';
