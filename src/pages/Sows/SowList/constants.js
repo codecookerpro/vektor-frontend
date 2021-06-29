f7683cd57599eb4d426597d8fe9b232dae7b6bc7
unknown <steve.jin.md@gmail.com>
2021-06-29 22:51:30 +0900

    [fix] sow list view permssion bug

---
+++ src/pages/Sows/SowList/constants.js
@@ -0,0 +1,3 @@
+import { PERMISSION_TYPES } from 'utils/constants';
+
+export const ALLOWED_PERMISSIONS = [PERMISSION_TYPES.admin, PERMISSION_TYPES.supervisor, PERMISSION_TYPES.projectManager, PERMISSION_TYPES.user];
