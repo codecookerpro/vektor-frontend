336ba4020dcd8fb90dc695c3c13bd791681424f1
unknown <steve.jin.md@gmail.com>
2021-07-02 06:51:24 +0900

    [fix] useFocusElement bug

--- src/utils/hooks/useFocusElement.js
+++ src/utils/hooks/useFocusElement.js
@@ -5,7 +5,7 @@ import { isEmpty } from 'utils/helpers/utility';
 const useFocusElement = (loadTarget = null) => {
   const { hash } = useLocation();
   useEffect(() => {
-    if (!isEmpty(loadTarget)) {
+    if (!isEmpty(loadTarget) && hash) {
       setTimeout(() => document.getElementById(hash.slice(1)).scrollIntoView(), 50);
     }
   }, [loadTarget, hash]);
