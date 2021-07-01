483e7d40545f89a31a70f959c77361a26eaed087
unknown <steve.jin.md@gmail.com>
2021-07-02 07:11:45 +0900

    [add] calculated fields below system form

--- src/pages/MetaSystems/Shared/MetaSystemForm/index.js
+++ src/pages/MetaSystems/Shared/MetaSystemForm/index.js
@@ -4,7 +4,20 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useForm, Controller } from 'react-hook-form';
 import { joiResolver } from '@hookform/resolvers/joi';
 import joi from 'joi';
-import { Card, CardContent, Grid, Button, Typography, Box } from '@material-ui/core';
+import {
+  Card,
+  CardContent,
+  Grid,
+  Button,
+  Typography,
+  Box,
+  TableContainer,
+  Table,
+  TableHead,
+  TableBody,
+  TableRow,
+  TableCell,
+} from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 
 import { ColorButton } from 'components/UI/Buttons';
@@ -58,6 +71,7 @@ const MetaSystemForm = ({ mode = FORM_MODE.view, system = {}, setFormMode = noop
     resolver: joiResolver(schema),
   });
   const fieldsDisabled = mode === FORM_MODE.view;
+  const calculated = system?.mainSystem?.calculated;
 
   const onSubmit = (data) => {
     const params = {
@@ -98,162 +112,194 @@ const MetaSystemForm = ({ mode = FORM_MODE.view, system = {}, setFormMode = noop
   };
 
   return (
-    <Card>
-      <CardContent>
-        <Typography variant="h6" className={classes.name}>
-          {system.name || 'New System'}
-        </Typography>
-        <form noValidate className={classes.form}>
-          <Grid container spacing={6}>
-            <Grid item xs={12} sm={6} md={4}>
-              <Controller
-                as={<VektorTextField />}
-                fullWidth
-                disabled={fieldsDisabled}
-                id="name"
-                name="name"
-                label="Name"
-                placeholder="Name"
-                error={errors.name?.message}
-                control={control}
-                defaultValue={system.name || ''}
-              />
-            </Grid>
-            <Grid item xs={12} sm={6} md={4}>
-              <Controller
-                as={<FilterSelect />}
-                fullWidth
-                disabled={fieldsDisabled}
-                name="equipmentCategory"
-                label="Equipment Category"
-                placeholder="Select Category"
-                items={EQUIPMENT_CATEGORIES}
-                error={errors.equipmentCategory?.message}
-                control={control}
-                defaultValue={system.equipmentCategory || EQUIPMENT_CATEGORIES[0].VALUE}
-              />
-            </Grid>
-            <Grid item xs={12} sm={6} md={4}>
-              <Controller
-                as={<FilterSelect />}
-                fullWidth
-                disabled={fieldsDisabled}
-                name="equipmentType"
-                label="Equipment Type"
-                placeholder="Select Type"
-                items={EQUIPMENTS}
-                error={errors.equipmentType?.message}
-                control={control}
-                defaultValue={system.equipmentType || EQUIPMENTS[0].VALUE}
-              />
-            </Grid>
-            <Grid item xs={12} sm={6} md={3}>
-              <Controller
-                as={<VektorTextField />}
-                fullWidth
-                disabled={fieldsDisabled}
-                id="equipmentName"
-                name="equipmentName"
-                label="Equipment Name"
-                placeholder="Equipment Name"
-                error={errors.equipmentName?.message}
-                control={control}
-                defaultValue={system.equipmentName || ''}
-              />
-            </Grid>
-            <Grid item xs={12} sm={6} md={3}>
-              <Controller
-                as={<VektorTextField />}
-                fullWidth
-                disabled={fieldsDisabled}
-                id="equipmentNumber"
-                name="equipmentNumber"
-                label="Equipment Number"
-                placeholder="Equipment Number"
-                error={errors.equipmentNumber?.message}
-                control={control}
-                defaultValue={system.equipmentNumber || ''}
-              />
-            </Grid>
-            <Grid item xs={12} sm={6} md={3}>
-              <Controller
-                as={<VektorTextField />}
-                fullWidth
-                disabled={fieldsDisabled}
-                id="productCode"
-                name="productCode"
-                label="Product Code"
-                placeholder="Product Code"
-                error={errors.productCode?.message}
-                control={control}
-                defaultValue={system.productCode}
-              />
-            </Grid>
-            <Grid item xs={12} sm={6} md={3}>
-              <Controller
-                as={<FilterSelect />}
-                fullWidth
-                disabled={fieldsDisabled}
-                name="organization"
-                label="Organization"
-                placeholder="Select Organization"
-                items={organizations}
-                keys={{
-                  label: 'name',
-                  value: '_id',
-                }}
-                error={errors.organization?.message}
-                control={control}
-                defaultValue={system.organization}
-              />
-            </Grid>
-            <Grid item xs={12} sm={6} md={3}>
-              <Controller
-                as={<VektorTextField />}
-                fullWidth
-                disabled={fieldsDisabled}
-                id="site"
-                name="site"
-                label="Site"
-                placeholder="Site"
-                error={errors.site?.message}
-                control={control}
-                defaultValue={system.site || ''}
-              />
-            </Grid>
+    <>
+      <Card>
+        <CardContent>
+          <Typography variant="h6" className={classes.name}>
+            {system.name || 'New System'}
+          </Typography>
+          <form noValidate className={classes.form}>
+            <Grid container spacing={6}>
+              <Grid item xs={12} sm={6} md={4}>
+                <Controller
+                  as={<VektorTextField />}
+                  fullWidth
+                  disabled={fieldsDisabled}
+                  id="name"
+                  name="name"
+                  label="Name"
+                  placeholder="Name"
+                  error={errors.name?.message}
+                  control={control}
+                  defaultValue={system.name || ''}
+                />
+              </Grid>
+              <Grid item xs={12} sm={6} md={4}>
+                <Controller
+                  as={<FilterSelect />}
+                  fullWidth
+                  disabled={fieldsDisabled}
+                  name="equipmentCategory"
+                  label="Equipment Category"
+                  placeholder="Select Category"
+                  items={EQUIPMENT_CATEGORIES}
+                  error={errors.equipmentCategory?.message}
+                  control={control}
+                  defaultValue={system.equipmentCategory || EQUIPMENT_CATEGORIES[0].VALUE}
+                />
+              </Grid>
+              <Grid item xs={12} sm={6} md={4}>
+                <Controller
+                  as={<FilterSelect />}
+                  fullWidth
+                  disabled={fieldsDisabled}
+                  name="equipmentType"
+                  label="Equipment Type"
+                  placeholder="Select Type"
+                  items={EQUIPMENTS}
+                  error={errors.equipmentType?.message}
+                  control={control}
+                  defaultValue={system.equipmentType || EQUIPMENTS[0].VALUE}
+                />
+              </Grid>
+              <Grid item xs={12} sm={6} md={3}>
+                <Controller
+                  as={<VektorTextField />}
+                  fullWidth
+                  disabled={fieldsDisabled}
+                  id="equipmentName"
+                  name="equipmentName"
+                  label="Equipment Name"
+                  placeholder="Equipment Name"
+                  error={errors.equipmentName?.message}
+                  control={control}
+                  defaultValue={system.equipmentName || ''}
+                />
+              </Grid>
+              <Grid item xs={12} sm={6} md={3}>
+                <Controller
+                  as={<VektorTextField />}
+                  fullWidth
+                  disabled={fieldsDisabled}
+                  id="equipmentNumber"
+                  name="equipmentNumber"
+                  label="Equipment Number"
+                  placeholder="Equipment Number"
+                  error={errors.equipmentNumber?.message}
+                  control={control}
+                  defaultValue={system.equipmentNumber || ''}
+                />
+              </Grid>
+              <Grid item xs={12} sm={6} md={3}>
+                <Controller
+                  as={<VektorTextField />}
+                  fullWidth
+                  disabled={fieldsDisabled}
+                  id="productCode"
+                  name="productCode"
+                  label="Product Code"
+                  placeholder="Product Code"
+                  error={errors.productCode?.message}
+                  control={control}
+                  defaultValue={system.productCode}
+                />
+              </Grid>
+              <Grid item xs={12} sm={6} md={3}>
+                <Controller
+                  as={<FilterSelect />}
+                  fullWidth
+                  disabled={fieldsDisabled}
+                  name="organization"
+                  label="Organization"
+                  placeholder="Select Organization"
+                  items={organizations}
+                  keys={{
+                    label: 'name',
+                    value: '_id',
+                  }}
+                  error={errors.organization?.message}
+                  control={control}
+                  defaultValue={system.organization}
+                />
+              </Grid>
+              <Grid item xs={12} sm={6} md={3}>
+                <Controller
+                  as={<VektorTextField />}
+                  fullWidth
+                  disabled={fieldsDisabled}
+                  id="site"
+                  name="site"
+                  label="Site"
+                  placeholder="Site"
+                  error={errors.site?.message}
+                  control={control}
+                  defaultValue={system.site || ''}
+                />
+              </Grid>
 
-            <Grid item xs={12}>
-              {mode === FORM_MODE.view ? (
-                <Box display="flex">
-                  <Button variant="contained" color="primary" onClick={() => setFormMode(FORM_MODE.update)}>
-                    EDIT
-                  </Button>
-                  <Button variant="contained" color="default" className={classes.button} onClick={handleDuplicate}>
-                    DUPLICATE
-                  </Button>
-                </Box>
-              ) : (
-                <Box display="flex">
-                  <Button variant="contained" color="primary" onClick={handleSubmit(onSubmit)}>
-                    SAVE CHANGES
-                  </Button>
-                  {mode === FORM_MODE.update && (
-                    <>
-                      <Button variant="contained" color="default" className={classes.button} onClick={() => setFormMode(FORM_MODE.view)}>
-                        CANCEL
-                      </Button>
-                      <ColorButton variant="contained" colour="red" className={classes.deleteButton} onClick={handleDelete}>
-                        DELETE
-                      </ColorButton>
-                    </>
-                  )}
-                </Box>
-              )}
+              <Grid item xs={12}>
+                {mode === FORM_MODE.view ? (
+                  <Box display="flex">
+                    <Button variant="contained" color="primary" onClick={() => setFormMode(FORM_MODE.update)}>
+                      EDIT
+                    </Button>
+                    <Button variant="contained" color="default" className={classes.button} onClick={handleDuplicate}>
+                      DUPLICATE
+                    </Button>
+                  </Box>
+                ) : (
+                  <Box display="flex">
+                    <Button variant="contained" color="primary" onClick={handleSubmit(onSubmit)}>
+                      SAVE CHANGES
+                    </Button>
+                    {mode === FORM_MODE.update && (
+                      <>
+                        <Button variant="contained" color="default" className={classes.button} onClick={() => setFormMode(FORM_MODE.view)}>
+                          CANCEL
+                        </Button>
+                        <ColorButton variant="contained" colour="red" className={classes.deleteButton} onClick={handleDelete}>
+                          DELETE
+                        </ColorButton>
+                      </>
+                    )}
+                  </Box>
+                )}
+              </Grid>
             </Grid>
-          </Grid>
-        </form>
-      </CardContent>
-    </Card>
+          </form>
+        </CardContent>
+      </Card>
+      {calculated && (
+        <Box mt={6}>
+          <Card>
+            <CardContent>
+              <TableContainer>
+                <Table>
+                  <TableHead>
+                    <TableRow>
+                      <TableCell>Total Planned Hours</TableCell>
+                      <TableCell>PV</TableCell>
+                      <TableCell>Status</TableCell>
+                      <TableCell>Effort</TableCell>
+                      <TableCell>EV</TableCell>
+                    </TableRow>
+                  </TableHead>
+                  <TableBody>
+                    <TableRow>
+                      <TableCell>{calculated.totalPlannedHours}</TableCell>
+                      <TableCell>{calculated.PV}%</TableCell>
+                      <TableCell>{calculated.status}%</TableCell>
+                      <TableCell>{Math.round(calculated.totalWorkedHours / calculated.totalPlannedHours) * 100}%</TableCell>
+                      <TableCell>{calculated.EV}%</TableCell>
+                    </TableRow>
+                  </TableBody>
+                </Table>
+              </TableContainer>
+            </CardContent>
+          </Card>
+        </Box>
+      )}
+    </>
   );
 };
 
